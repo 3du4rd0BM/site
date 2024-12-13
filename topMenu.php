fcd8727969d5a34c6968148e97a93891e5ee47f6
Eduardo Blanco M. <zener.servitec@gmail.com>
2024-12-13 15:56:39 -0300

    clean

--- topMenu.php
+++
@@ -1,6 +0,0 @@
-<li><a href="../home">Home</a></li>
-        <li><a href="../localiza">Localização</a></li>
-        <li><a href="../downloads">Downloads</a></li>
-        <li><a href="https://zenertec.sualojaonline.app/">Loja</a></li>
-        <li><a href="../../moodle">Cursos</a></li>
-        <li><a href="../../moodle/login">Alunos</a></li>
