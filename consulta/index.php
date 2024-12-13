49f4d909578a4ca3fce7db56ee9ddc770385c40a
Eduardo Blanco M. <zener.servitec@gmail.com>
2024-12-13 14:51:53 -0300

    Primeiro

---
+++ consulta/index.php
@@ -0,0 +1,10 @@
+<?php include '../top.php';?>
+ <!--  <div class="container center">
+  	<div class="section">
+  		<div class="row">
+  			
+  		</div>
+     </div>
+  </div>
+   -->
+<?php include '../bottom.php';?>
---
+++ top.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="icon" href="../favicon.png" sizes="32x32">
+  <title>ZENER Serviço Técnico</title>
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+<div class="navbar-fixed">
+  <nav class="grey lighten-3" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo"><img alt="" src="../zener_preto.png"></a>
+      <ul class="right hide-on-med-and-down">
+        <?php include '../topMenu.php';?>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger black-text"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+</div>
+ 	  <ul id="nav-mobile" class="sidenav">
+        <?php include '../topMenu.php';?>
+      </ul>
